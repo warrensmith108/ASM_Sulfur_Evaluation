b38ebbd9168ae2956377177a339e9be6e0e7cecc
Ren Smith <wsmith@ucar.edu>
2026-01-06 08:58:14 -0700

    Added files via upload

---
+++ sim_name_remap.py
@@ -0,0 +1,81 @@
+
+
+remap = \
+	{
+
+# ##################### MAM4 CAMS
+
+'MAM4_CAMS6.2_SO2':'/glade/campaign/acom/acom-climate/wsmith/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_cams6.2_withbb.acclip/FC*h1.2022-08*', 
+# Previous location: '/glade/derecho/scratch/wsmith/archive/ FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_cams6.2_withbb.acclip/atm/hist/FC*h1.2022-08*',      #'MAM4_CAMS6.2_SO2':'/glade/derecho/scratch/wsmith/archive/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_cams6.2.acclip/atm/hist/FC*h1.2022-08*',  # This run did not have the correct biomass burning emissions
+
+'MAM4_CAMS6.2_SO2_rad':'/glade/campaign/acom/acom-climate/wsmith/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_cams6.2_withbb.rad/FC*h0*', 
+
+# ##################### MAM4 MEIC
+
+'MAM4_MEIC_SO2':'/glade/campaign/acom/acom-climate/wsmith/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic_withbb.acclip/FC*h1.2022-08*',
+#Previous location '/glade/derecho/scratch/wsmith/archive/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic_withbb.acclip/atm/hist/FC*h1.2022-08*',
+#'MAM4_MEIC_SO2':'/glade/derecho/scratch/wsmith/archive/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic.acclip.out01/atm/hist/FC*h1.2022-08*',  # This run did not have the correct biomass burning emissions
+
+'MAM4_MEIC_SO2_rad':'/glade/campaign/acom/acom-climate/wsmith/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic_withbb.rad/FC*h0*',
+
+# #################### CARMA CAMS
+
+'CARMA_CAMS_SO2':'/glade/campaign/acom/acom-climate/wsmith/FCARMAnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_cams6.2_withbb.acclip/F*h1.2022-08*',
+'CARMA_CAMS_SO2_bins':'/glade/derecho/scratch/wsmith/archive/FCARMAnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_cams6.2_withbb.acclip1/atm/hist/F*h1.2022-08*',        
+#'/glade/campaign/acom/acom-climate/wsmith/FCARMAnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_cams6.2.acclip/F*h1.2022-08*',
+# Previous location: '/glade/derecho/scratch/wsmith/archive/FCARMAnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_cams6.2.acclip/atm/hist/F*h1.2022-08*',
+#'CARMA_CAMS_SO2':'/glade/derecho/scratch/tilmes/archive/FCARMAnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_cams6.2/atm/hist/F*',  # Simone's 
+
+'CARMA_CAMS_SO2_rad':'/glade/campaign/acom/acom-climate/wsmith/FCARMAnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_cams6.2.002.rad/FC*h0*',
+
+# 'CARMA_CAMS_SO2_Aug22':'/glade/campaign/acom/acom-climate/wsmith/FCARMAnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_cams6.2.002.rad/FC*h0.2022-08*',    # Sadly I don't think this file exists anymore
+
+# ####################  CARMA MEIC
+
+'CARMA_MEIC_SO2':'/glade/campaign/acom/acom-climate/wsmith/FCARMAnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic_withbb.acclip/F*h1.2022-08*',
+'CARMA_MEIC_SO2_bins':'/glade/derecho/scratch/wsmith/archive/FCARMAnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic_withbb.acclip1/atm/hist/F*h1.2022-08*',
+#'/glade/campaign/acom/acom-climate/wsmith/FCARMAnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic.acclip/F*h1.2022-08*',
+# Previous location: '/glade/derecho/scratch/wsmith/archive/FCARMAnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic.acclip/atm/hist/F*h1.2022-08*',
+#'CARMA_MEIC_SO2':'/glade/derecho/scratch/tilmes/archive/FCARMAnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic/atm/hist/F*',  # Simone's
+
+#'CARMA_MEIC_SO2_rad':'/glade/campaign/acom/acom-climate/tilmes/CARMA/ACCLIP_SABRE/FCARMAnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic.002/atm/hist/FC*h0*',
+'CARMA_MEIC_SO2_rad':'/glade/campaign/acom/acom-climate/wsmith/FCARMAnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic.002.rad/FC*h0*',
+
+'CARMA_MEIC_SO2_Aug22':'/glade/campaign/acom/acom-climate/wsmith/FCARMAnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic.Aug22/FC*h0.2022-08*',    
+
+# ######################
+
+'MAM4_MEIC_110L':'/glade/campaign/acom/acom-climate/wsmith/FWnudged_f09_110L.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic.acclip/FW*h1.2022-08*',
+# Previous location: '/glade/derecho/scratch/wsmith/archive/FWnudged_f09_110L.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic.acclip/atm/hist/FW*h1.2022-08*',    # changed to wsmith archive
+
+'MAM4_MEIC_SO2_ZMKE':'/glade/derecho/scratch/wsmith/archive/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic_withbb_RichConv.002.acclip/atm/hist/FC*h1.2022-08*',
+
+'MAM4_CESM3_test':'/glade/derecho/scratch/tilmes/archive/f.e30.alpha07e.ctsm5.3.075.sp.nudged.2016_2018_MTt4s.ne30.002/atm/hist/f.e30*.h0a.2022-08*',
+
+        
+
+# These runs are less important
+
+'MAM4_MEIC_SO21':'/glade/derecho/scratch/wsmith/archive/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic_withbb.acclip1/atm/hist/FC*h1.2022-08*',
+
+'MAM4_MEIC_SO2_ZMKE1':'/glade/derecho/scratch/wsmith/archive/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic_withbb_RichConv.002.acclip1/atm/hist/FC*h1.2022-08*',
+
+'MAM4_MEIC_SO2_monthly':'/glade/derecho/scratch/wsmith/archive/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic.acclip.out01/atm/hist/FC*h0.2022-08*',
+    'MAM4_MEIC_factb':'/glade/derecho/scratch/wsmith/archive/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic.acclip.factb/atm/hist/FC*h1.2022-08*',
+    'MAM4_MEIC_facti':'/glade/derecho/scratch/wsmith/archive/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic.acclip.facti/atm/hist/FC*h1.2022-08*',        
+    'MAM4_CAMS6.2_SO2_noChina':'/glade/derecho/scratch/wsmith/archive/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_cams6.2_noChina.acclip/atm/hist/FC*h1.2022-08*',
+    'MAM4_CAMS6.2_SO2_noBCair':'/glade/derecho/scratch/wsmith/archive/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_cams6.2_noBCair/atm/hist/FC*h1.2022-08*',
+    'MAM4_MEIC_110L_1':'/glade/derecho/scratch/wsmith/archive/FWnudged_f09_110L.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic.acclip1/atm/hist/FW*h1.2022-08*',    # changed to wsmith archive        
+    'MAM4_MEIC_hygro':'/glade/derecho/scratch/tilmes/archive/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic.acclip.hygro0.9/atm/hist/FC*h1.2022-08*',
+
+    'MAM4_MEIC_all-aer-act':'/glade/derecho/scratch/tilmes/archive/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic.acclip.conv1/atm/hist/FC*h1.2022-08*',
+
+    'MAM4_MEIC_no-aer-act':'/glade/derecho/scratch/tilmes/archive/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic.acclip.conv2/atm/hist/FC*h1.2022-08*',
+
+'2010_Convection-KE':'/glade/derecho/scratch/rneale/archive/f.cam6_3_161.FLTHIST_ne30.ke.001_hf/atm/hist/f*h3i.2010-08*',
+
+'ZM_KE_Test_Off':'/glade/derecho/scratch/wsmith/archive/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic_withbb_RichConv.001/atm/hist/FC*h1.2010-01-05*',
+
+'ZM_KE_Test_On':'/glade/derecho/scratch/wsmith/archive/FCnudged_f09.carmats16_cesm2.2.carma_trop_strat.2010_2021_cams5.3_so2_meic_withbb_RichConv.002/atm/hist/FC*h1.2010-01-05*'
+        
+    }
